f2030124c9a8af8bb0cfcad5745d94c13b525421
rajahariadi <rajahariadi0@gmail.com>
2024-02-09 03:58:13 +0700

    Kelurahan Data Table

--- app/Livewire/Master/Kelurahan.php
+++ app/Livewire/Master/Kelurahan.php
@@ -3,9 +3,31 @@
 namespace App\Livewire\Master;
 
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class Kelurahan extends Component
 {
+
+    use WithFileUploads;
+
+    public $file_import;
+
+    public function rules()
+    {
+        return [
+            "file_import" => "required|mimes:xlsx",
+        ];
+    }
+
+    protected $validationAttributes = [
+        "file_import" => "File Excel",
+    ];
+
+    public function importData()
+    {
+        $this->validate();
+        dd('okee');
+    }
     public function render()
     {
         return view('mods.master.kelurahan_data');
